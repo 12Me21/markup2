c3100a28d3eca9dd03fb3c7dfcb46934ab5ea6ac
12Me21 <12Me21.mc@gmail.com>
2022-03-18 18:26:56 -0400

    split-based parser

--- parser.js
+++ parser.js
@@ -1,108 +1,54 @@
-function parse2(code) {
-	let i = 0
-	let lines = []
-	while (1) {
-		let [ok, next, res] = parse(code, i, null)
-		lines.push(res)
-		if (ok >= 3)
-			break
-		i = next
+let tags = [
+	/[/]/,
+	/[**]/,
+	/[\\]\w+/,
+	/`(?:[^`]|``)*`/,
+]
+let r = new RegExp("("+tags.map(tag=>tag.source+"()").join("|")+")")
+
+function parse(text) {
+	let spl = String.prototype.split.call(text, r)
+	spl = filter_tags(spl)
+	for (let i=0; i<spl.length; i+=3) {
+		console.log(spl[i])
+		console.log(spl[i+1], spl[i+2])
 	}
-	return lines
 }
 
-function parse(code, start, blocktype) {
-	console.log("parse called: ", start, blocktype)
-	
-	let tree = []
-	// todo: maybe store text as a range until
-	let text_buffer = ""
-	function add_text(text) {
-		text_buffer += text
+function check_tag(text, tag, type) {
+	switch (type) {
+	case 0:
+		if (/\w$/.test(text))
+			return 'italic_end'
+		return 'italic_start'
+	case 1:
+		if (/\w$/.test(text))
+			return 'bold_end'
+		return 'bold_start'
+	case 2:
+		return 'tag_start'
+	case 3:
+		return 'icode'
 	}
-	
-	function flush_text() {
-		if (text_buffer) {
-			tree.push(text_buffer)
-			text_buffer = ""
-		}
-	}
-	
-	function insert(x) {
-		flush_text()
-		if (x instanceof Array)
-			tree.push(...x) // array
-		else
-			tree.push(x) // object or text
-	}
-	
-	let c
-	function scan() {
-		i++
-		c = code.charAt(i)
-	}
-	// is this safe? when we start dealing with like linebreaks idk
-	function restore(pos) {
-		i = pos-1
-		scan()
-	}
-	
-	function finish(ok, type) {
-		flush_text()
-		if (type)
-			tree = {type: type, contents: tree}
-		return [ok, i, tree]
-	}
-	
-	restore(start)
-	
-	while (c) {
-		if (c=="*") {
-			if (blocktype=='bold') {
-				scan()
-				return finish(0, blocktype)
-			} else {
-				let [ok, next, res] = parse(code, i+1, 'bold')
-				if (ok==0) {
-					scan()
-				} else {
-					add_text(c)
-					scan()
-				}
-				insert(res)
-				restore(next)
-			}
-		} else if (c=="/") {
-			if (blocktype=='italic') {
-				scan()
-				return finish(0, blocktype)
-			} else {
-				let [ok, next, res] = parse(code, i+1, 'italic')
-				if (ok==0) {
-					scan()
-				} else {
-					add_text(c)
-					scan()
-				}
-				insert(res)
-				restore(next)
-			}
-	/*	} else if (c=='\n') {
-			add_text(c)
-			scan()
-			if (blocktype!='bold')
-				return finish(2, null)*/
+	return true
+}
+
+function filter_tags(spl) {
+	let list = []
+	let bac = ""
+	let i;
+	for (i=0; i<spl.length-1; i+=tags.length+2) {
+		let text = spl[i]
+		let tag = spl[i+1]
+		let type = spl.indexOf("", i+2) - (i+2)
+		type = check_tag(text, tag, type)
+		if (type!=null) {
+			list.push(bac+text, tag, type)
+			bac = ""
 		} else {
-			add_text(c)
-			scan()
+			bac += text + tag
 		}
 	}
-	console.log("reached end", blocktype)
-	return finish(3, null)
+	list.push(bac+spl[i])
+	return list
 }
-
-// statuses
-// 1: ok
-// 2: fail
-// 3: eol
-// 4: eof
