181d18ec5c62088eadfebd97a1acd968f713f971
12Me21 <12Me21.mc@gmail.com>
2022-03-18 20:28:35 -0400

    testing more types in the rege

--- parser.js
+++ parser.js
@@ -1,21 +1,47 @@
 let tags = [
-	/[/]/,
-	/[**]/,
-	/[\\]\w+/,
-	/`(?:[^`]|``)*`/,
+	/\n/, // newline
+	
+	/#{1,3} /, // heading
+	/^---+$/, //hr
+	
+	/[*][*]/, // bold
+	/[/]/, // italic
+	/__/, // underline
+	/~~/, // strikethrough
+	
+	/[\\](?:[{]|\w+(?:\[.*?\])?[{]?)/, // env
+	/[}]/, // env end
+	/[\\][^]/, // escaped char
+	
+	/`.*?`/, // icode
+	/```[^]*?```/, // code
+	
+	/!?(?:https?:[/][/]|sbs:)[-\w./%?&=#+~@:$*',;!]*(?:[-\w/%&=#+~@$*';]|[(][-\w./%?&=#+~@:$*',;!]*[)](?:[-\w./%?&=#+~@:$*',;!]*[-\w/%&=#+~@$*';])?)(?:\[.*?\])?[{]?/, // url - UE | U(U) | U(U)UE
+	
+	/ *(?:[|]\n)?[|](?:\[.*?\])? */, // table start, table next row, table next cell
+	/ *[|] *$/, // table end
+	
+	/^ *- /, //list
 ]
-let r = new RegExp("("+tags.map(tag=>tag.source+"()").join("|")+")")
+//[-\w/%&=#+~@$*';]
+//[-\w./%?&=#+~@:$*',;!]
+let r = new RegExp("("+tags.map(tag=>tag.source+"()").join("|")+")", 'm')
 
 function parse(text) {
+	x=performance.now()
 	let spl = String.prototype.split.call(text, r)
 	spl = filter_tags(spl)
+	console.log(performance.now() - x)
 	for (let i=0; i<spl.length; i+=3) {
 		console.log(spl[i])
 		console.log(spl[i+1], spl[i+2])
 	}
 }
 
+// heck *asfasfasf ... whatever
+
 function check_tag(text, tag, type) {
+	return ['newline', 'heading', 'line', 'bold', 'italic', 'underline', 'strikethrough', 'env', 'env_end', 'escape', 'icode', 'code', 'link', 'table', 'table_end', 'list'][type]
 	switch (type) {
 	case 0:
 		if (/\w$/.test(text))
