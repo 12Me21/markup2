6878f6f5b4aa89e77f014dd4eb7d0d3691fb9975
12Me21 <12Me21.mc@gmail.com>
2022-03-23 15:59:05 -0400

    n

--- render.js
+++ render.js
@@ -82,13 +82,6 @@ Markup.render = (function(){
 		
 		let last_block = false
 		
-		// todo: what if instead of all this newline mess, we just
-		// parsed, ex
-		// > quote
-		// as: \n> quote\n
-		// i.e. eat the newlines DURING PARSING
-		// well, really it would be (\n|^)> quote(\n|$)
-		
 		if (tree.content!=undefined) {
 			let got_newline = false
 			for (let i of tree.content) {
@@ -99,13 +92,10 @@ Markup.render = (function(){
 					branch.append(i)
 					last_block = false
 				} else if (i.type=='newline') {
-					if (got_newline) {
-						if (!last_block)
-							branch.append(blocks.newline())
-						branch.append("")
-						last_block = false
-					}
+					if (got_newline && !last_block)
+						branch.append(blocks.newline())
 					got_newline = true
+					last_block = false
 				} else {
 					let [elem, is_block] = render_branch(i)
 					if (got_newline) {
