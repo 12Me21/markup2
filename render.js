c89730a477a52fb84ae9e299472c731da1bc14c5
12Me21 <12Me21.mc@gmail.com>
2022-03-30 01:19:47 -0400

    new template system!!

--- render.js
+++ render.js
@@ -1,10 +1,6 @@
 Markup.IMPORT = EXPORT=>{
 	"use strict"
 	
-	let creator = document.createElement.bind.bind(document.createElement, document)
-	let elem = document.createElement.bind(document)
-	let frag = document.createDocumentFragment.bind(document)
-	
 	let is_block = {
 		code:'block', line:'block', ROOT:'block', heading:'block', quote:'block', table:'block',
 		table_cell:'block',
@@ -12,193 +8,146 @@ Markup.IMPORT = EXPORT=>{
 	
 	function 𐀶([html]) {
 		let t = document.createElement('template')
-		t.innerHTML = html
-		return t.content
-	}
-	
-	let TEMPLATES = {
-		newline: 𐀶`<br>`,
-		divider: 𐀶`<hr>`,
-		code: 𐀶`<pre>`,
-		icode: 𐀶`<icode>`,
-		simple_link: 𐀶`<a>`,
-		image: 𐀶`<img data-loading data-shrink tabindex=-1>`,
-		error: 𐀶`<div class='error'><code>🌱error🌱</code><br id=message><pre><br id=stack>`,
-		audio: 𐀶`<audio controls preload=none>`,
-		italic: 𐀶`<i>`,
-		bold: 𐀶`<b>`,
-		strikethrough: 𐀶`<s>`,
-		underline: 𐀶`<u>`,
-		heading: null, // how are we... maybe have an override or
-		quote: 𐀶`<blockquote><cite>🌱cite🌱`,
-		table: 𐀶`<table><tbody>`,
-		table_row: 𐀶`<tr>`,
-		table_cell: null,
-		link: 𐀶`<a>`,
-		list: null,
-		list_item: 𐀶`<li>`,
-		align: 𐀶`<div>`,
-		subscript: 𐀶`<sub>`,
-		superscript: 𐀶`<sup>`,
-		anchor: 𐀶`<a>`,
-		ruby: 𐀶`<ruby><br id=inside><rp>(<rt><br id=top><rp>(`,
-		spoiler: 𐀶`<button class=spoilerButton><br id=button></button><div class=spoiler><br id=inside>`,
-		background_color: 𐀶`<span>`,
+		t.innerHTML = html//.replace(/🕯\w+🕯/g, "<br id=$&>").replace(/>🌿/g, " id=B>")
+		let c = t.content
+		if (c.childNodes.length==1)
+			c = c.childNodes[0]
+		return c.cloneNode.bind(c, true)
 	}
 	
 	let CREATE = {
-		// blocks without children:
-		newline: creator('br'),
-		divider: creator('hr'),
-		code({text, lang}) {
-			let x = elem('pre')
-			x.textContent = args
+		newline: 𐀶`<br>`,
+		
+		divider: 𐀶`<hr>`,
+		
+		code: function({text, lang}) {
+			let x = this()
+			x.textContent = text
 			return x
-		},
-		icode({text}) {
-			let x = elem('code')
-			x.textContent = args.replace(/ /g, " ")
+		}.bind(𐀶`<pre>`),
+		
+		icode: function({text}) {
+			let x = this()
+			x.textContent = text.replace(/ /g, " ")
 			return x
-		},
-		simple_link({url, text}) {
-			let x = elem('a')
+		}.bind(𐀶`<code>`),
+		
+		simple_link: function({url, text}) {
+			let x = this()
 			x.textContent = text
 			x.href = url
 			return x
-		},
-		image({url, alt, width, height}) {
-			let e = elem('img')
-			e.src = url
-			e.dataset.loading = ""
-			e.dataset.shrink = ""
-			e.onerror = e.onload = ()=>{
-				delete e.dataset.loading
-			}
-			e.tabIndex = -1
-			if (alt!=null) e.alt = alt
-			if (width) e.width = width
-			if (height) e.height = height
-			return e
-		},
-		error({error, stack}) {
-			let node = elem('div')
-			node.className = "error"
-			let err = elem('code')
-			err.textContent = error
-			node.append("Markup parsing error: ", err, "\nPlease report this!")
-			if (stack) {
-				let pre = elem('pre')
-				pre.textContent = stack
-				node.append(pre)
-			}
-			return node
-		},
-		audio({url}) {
-			let node = elem('audio')
-			node.controls = true
-			node.src = url
-			node.preload = 'none'
-			return node
-		},
-		// blocks with children:
-		ROOT: frag,
-		italic: creator('i'),
-		bold: creator('b'),
-		strikethrough: creator('s'),
-		underline: creator('u'),
-		heading({level}) {
-			return elem("h"+level)
-		},
-		quote({cite}) {
-			let x = elem('blockquote')
-			if (cite!=null) {
-				let c = elem('cite')
-				c.textContent = cite
-				x.append(c)
-			}
+		}.bind(𐀶`<a href="">`),
+		
+		image: function({url, alt, width, height}) {
+			let x = this()
+			x.src = url
+			x.onerror = x.onload = x.removeAttribute.bind(x, 'loading')
+			if (alt!=null) x.alt = alt
+			if (width) x.width = width
+			if (height) x.height = height
 			return x
-		},
-		table() {
-			let x = elem('table')
-			let y = elem('tbody')
-			x.append(y)
-			x.B = y
+		}.bind(𐀶`<img data-loading data-shrink tabindex=-1>`),
+		
+		error: 𐀶`<div class='error'><code>🕯error🕯</code>🕯message🕯<pre>🕯stack🕯`,
+		audio: 𐀶`<audio controls preload=none>`,
+		
+		italic: 𐀶`<i>`,
+		
+		bold: 𐀶`<b>`,
+		
+		strikethrough: 𐀶`<s>`,
+		
+		underline: 𐀶`<u>`,
+		
+		heading: function({level}) {
+			return this[level-1]()
+		}.bind([𐀶`<h1>`,𐀶`<h2>`,𐀶`<h3>`,𐀶`<h4>`]),
+		
+		quote: function({cite}) {
+			if (cite==null)
+				return this[0]()
+			let x = this[1]()
+			let cite = x.getElementById('cite')
+			cite.id = ""
+			cite.textContent = cite
 			return x
-		},
-		table_row: creator('tr'),
-		table_cell({header, color, truecolor, colspan, rowspan, align}) {
-			let e = elem(header ? 'th' : 'td')
+		}.bind([𐀶`<blockquote>`, 𐀶`<blockquote><cite id=cite>`]),
+		
+		table: 𐀶`<table>`, // todo: <tbody> ?
+		
+		table_row: 𐀶`<tr>`,
+		
+		table_cell: function({header, color, truecolor, colspan, rowspan, align}) {
+			let e = this[header?1:0]()
 			if (color) e.dataset.bgcolor = color
 			if (truecolor) e.style.backgroundColor = truecolor
 			if (colspan) e.colSpan = colspan
 			if (rowspan) e.rowSpan = rowspan
 			if (align) e.style.textAlign = align
 			return e
-		},
-		link({url}) {
-			let x = elem('a')
+		}.bind([𐀶`<td>`,𐀶`<th>`]),
+		
+		link: function({url}) {
+			let x = this()
 			x.href = url
 			return x
-		},
-		list({style}) {
+		}.bind(𐀶`<a target=_blank href=""></a>`),
+		
+		list: function({style}) {
 			if (style==null)
-				return elem('ul')
-			let list = elem('ol')
-			list.style.listStyleType = style
-			return list
-		},
-		list_item: creator('li'),
-		align({align}) {
-			let e = elem('div')
+				return this[0]()
+			let x = this[1]()
+			x.style.listStyleType = style
+			return x
+		}.bind([𐀶`<ul>`, 𐀶`<ol>`]),
+		
+		list_item: 𐀶`<li>`,
+		
+		align: function({align}) {
+			let e = this()
 			e.style.textAlign = align
 			return e
-		},
-		subscript: creator('sub'),
-		superscript: creator('sup'),
-		anchor({name}) {
-			let e = elem('a')
+		}.bind(𐀶`<div>`),
+		
+		subscript: 𐀶`<sub>`,
+		
+		superscript: 𐀶`<sup>`,
+		
+		anchor: function({name}) {
+			let e = this()
 			e.name = "_anchor_"+name
 			return e
-		},
-		ruby({text}) {
-			let e = elem('ruby')
-			let first = elem('span')
-			let x1 = elem('rp')
-			x1.textContent = "("
-			let x2 = elem('rt')
-			x2.textContent = text
-			let x3 = elem('rp')
-			x3.textContent = ")"
-			e.append(first, x1, x2, x3)
-			e.B = first
+		}.bind(𐀶`<a name="">`),
+		
+		ruby: function({text}) {
+			let e = this()
+			let branch = this.getElementById('branch')
+			let top = this.getElementById('top')
+			this.branch.id = ""
+			this.top.id = ""
+			top.textContent = text
+			e.B = branch
 			return e
-		},
-		spoiler({label}) {
-			let button = elem('button')
-			button.className = 'spoilerButton'
-			button.onclick = ()=>{
-				if (button.dataset.show == null)
-					button.dataset.show = ""
-				else
-					delete button.dataset.show
-			}
-			button.textContent = label
-			
-			let box = elem('div')
-			box.className = "spoiler"
-			
-			let node = frag()
-			node.append(button, box)
-			node.B = box
-			
-			return node
-		},
-		background_color({color}) {
-			let e = elem('span')
+		}.bind(𐀶`<ruby><span id=branch></span><rp>(<rt id=top><rp>(`),
+		
+		spoiler: function({label}) {
+			let x = this()
+			let btn = x.getElementById('button')
+			let branch = x.getElementById('branch')
+			btn.id = ""
+			branch.id = ""
+			btn.onclick = btn.toggleAttribute.bind(btn, 'data-show') // could toggle attribute on branch instead?
+			x.B = branch
+			return x
+		}.bind(𐀶`<button class=spoiler-button id=button></button><div id=branch>`),
+		
+		background_color: function({color}) {
+			let e = this()
 			if (color)
 				e.dataset.bgcolor = color
 			return e
-		},
+		}.bind(𐀶`<span>`),
 	}
 	
 	function fill_branch(branch, leaves) {
@@ -232,14 +181,10 @@ Markup.IMPORT = EXPORT=>{
 	}
 	
 	EXPORT.render = function(tree) {
-		let root = frag()
+		let root = document.createDocumentFragment()
 		fill_branch(root, tree.content)
 		return root
 	}
 	
-	EXPORT.TEMPLATES = TEMPLATES
-	
 	EXPORT.create = CREATE
 }
-
-//let append = Node.appendChild.call
