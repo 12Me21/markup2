fc1661e5a0142ba9ef8a7a24c0e846b9571de510
ilovecherries <cherry@epochal.quest>
2022-04-22 10:43:32 -0400

    ahhh im dumb

--- render.d.ts
+++ render.d.ts
@@ -1,10 +1,5 @@
 import type { MarkupAST } from "./langs";
 
-type RenderArgs = {
-	args: any,
-	content: MarkupAST,
-}
-
 export class Markup_Render_Html {
-	render(args: RenderArgs, node=document.createDocumentFragment()): DocumentFragment
+	render(args: MarkupAST, node=document.createDocumentFragment()): DocumentFragment
 }
