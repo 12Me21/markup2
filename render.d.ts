68422e6146f2746b6cf2dbad0fea6b3212093b2a
ilovecherries <cherry@epochal.quest>
2022-04-23 08:55:24 -0400

    expose leaf functions

--- render.d.ts
+++ render.d.ts
@@ -1,5 +1,10 @@
 import type { MarkupAST } from "./langs";
 
+export type LeafFunctions = {
+	[key: string]: (args: any) => HTMLElement
+};
+
 export class Markup_Render_Html {
 	render(args: MarkupAST, node=document.createDocumentFragment()): DocumentFragment
+	create: LeafFunctions;
 }
